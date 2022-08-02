8467d4d461c7fe620b7f082843129e0f1763f4e1
UmamiAppearance <mail@umamiappearance.eu>
2022-08-02 23:26:51 +0200

    update

--- .link-libs.js
+++ .link-libs.js
@@ -1,9 +1,26 @@
 import { link } from "fs";
-    
-link("./node_modules/contodo/themes/default.css", "./css/contodo.css", (err) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log("\nHard link created\n");
-    }
-});
+
+const copyLib = (path, dest) => {
+    link(path, dest, (err) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log(`Hard link created: '${path}' -> '${dest}'`);
+        }
+    });
+}
+
+copyLib(
+    "./node_modules/codejar/codejar.js",
+    "./lib/codejar.js"
+);
+
+copyLib(
+    "./node_modules/contodo/dist/no-style/contodo-no-style.esm.js",
+    "./lib/contodo.js"
+);
+
+// An automatic build for 'Prism.js' is not available.
+// You can download it here:
+// https://prismjs.com/download.html#themes=prism&languages=clike+javascript
+
