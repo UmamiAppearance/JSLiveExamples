008ce34830dd5c97961fe9501fd185e33fedff21
UmamiAppearance <mail@umamiappearance.eu>
2022-09-12 10:38:17 +0200

    v0.3.0

--- dist/no-style/js-live-examples-no-style.iife.js
+++ dist/no-style/js-live-examples-no-style.iife.js
@@ -1900,7 +1900,7 @@ var liveExamples = (function () {
 	/**
 	 * [contodo]{@link https://github.com/UmamiAppearance/contodo}
 	 *
-	 * @version 0.2.5
+	 * @version 0.2.6
 	 * @author UmamiAppearance [mail@umamiappearance.eu]
 	 * @license GPL-3.0
 	 */
