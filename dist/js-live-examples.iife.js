9f98b809fdc4e0a4fbb6aee74e10e75d5578040b
UmamiAppearance <mail@umamiappearance.eu>
2023-05-23 01:03:32 +0200

    v0.5.0

--- dist/js-live-examples.iife.js
+++ dist/js-live-examples.iife.js
@@ -1917,7 +1917,7 @@ var liveExamples = (function () {
 	/**
 	 * [contodo]{@link https://github.com/UmamiAppearance/contodo}
 	 *
-	 * @version 0.4.4
+	 * @version 0.4.5
 	 * @author UmamiAppearance [mail@umamiappearance.eu]
 	 * @license MIT
 	 */
